03b452f19e5d9788202a6553f1821a48e5d323ba
utah iev <uiev@csod.com>
2017-09-29 14:50:08 -0700

    Cleanup

---
+++ csod-edge-integrations-custom-provider-service/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace csod_edge_integrations_custom_provider_service
+{
+    public static class Extensions
+    {
+        public static int GetUserId(this ClaimsPrincipal principle)
+        {
+            return int.Parse(principle.Claims.Single(x => x.Type == "id").Value);
+        }
+    }
+}
